bf99156d3c105137869266eed328c622c725a2ee
bingwang <bingwang_tust@126.com>
2020-08-30 10:22:39 +0800

    增加部分离线算子计算案例

---
+++ src/main/scala/com/wb/data/wikiedits/WordCountScala.scala
@@ -0,0 +1,25 @@
+package com.wb.data.wikiedits
+
+import org.apache.flink.api.scala.{DataSet, ExecutionEnvironment, _}
+
+object WordCountScala {
+
+  def main(args: Array[String]): Unit = {
+    val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+
+    val text: DataSet[String] = env.fromElements(
+    "Who’s there?",
+    "I think I hear them. Stand, ho! Who’s there?")
+
+    val wordCounts: AggregateDataSet[(String, Int)] = text.flatMap(_.split("\\W+")).map((_, 1)).groupBy(0).sum(1)
+//    val wordCounts: AggregateDataSet[WordWithCount] = text.flatMap(_.split("\\W+"))
+//      .map(WordWithCount(_, 1))
+//      .groupBy(0)
+//      .sum("count")
+
+    wordCounts.print()
+
+  }
+}
+
+case class WordWithCount(word: String, count: Int)
